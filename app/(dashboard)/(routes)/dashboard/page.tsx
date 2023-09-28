266d93c5560ab7b3d926884512d835d8455f044a
Richard Emijere <67247138+The-CodeINN@users.noreply.github.com>
2023-09-28 14:09:44 +0000

    Working on the dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,6 +2,39 @@ import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Dashboard = () => {
+  const data = [
+    {
+      id: 1,
+      title: 'Salary',
+      amount: 100000,
+      type: 'income',
+    },
+    {
+      id: 2,
+      title: 'Rent',
+      amount: 100000,
+      type: 'expense',
+    },
+    {
+      id: 3,
+      title: 'Salary',
+      amount: 100000,
+      type: 'income',
+    },
+    {
+      id: 4,
+      title: 'Debt',
+      amount: 100000,
+      type: 'expense',
+    },
+    {
+      id: 5,
+      title: 'Salary',
+      amount: 100000,
+      type: 'income',
+    },
+  ];
+
   return (
     <div>
       <section className='py-3'>
@@ -19,7 +52,26 @@ const Dashboard = () => {
       {/* list of transactions */}
       <section className='py-6'>
         <h3 className='text-2xl font-bold'>My Transactions</h3>
-        <div className='flex flex-col gap-4 mt-6'></div>
+        <div className='flex flex-col gap-4 mt-6'>
+          {data.map((data) => (
+            <div key={data.id}>
+              <Button className='w-full py-4 flex justify-between items-center'>
+                <div>{data.title}</div>
+                <div>
+                  {data.type === 'income' ? (
+                    <span className='text-green-500 text-xl font-bold'>
+                      {data.amount}
+                    </span>
+                  ) : (
+                    <span className='text-red-500 text-xl font-bold'>
+                      {data.amount}
+                    </span>
+                  )}
+                </div>
+              </Button>
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   );
